1ffc12b8c7fd26b3f97c496be838c28fcf8cfea9
Bernard Kobina Forson Essel <108433679+EsselKobby@users.noreply.github.com>
2023-11-21 10:02:40 +0000

    Delete edx_python/Project0 directory

--- edx_python/Project0/indoor.py
+++
@@ -1,5 +0,0 @@
-user_input = input("Please Enter Your Input: ")
-
-user_input = user_input.strip().lower()
-
-print(user_input)
--- edx_python/Project0/playback.py
+++
@@ -1,6 +0,0 @@
-user_input = input("Please Enter Your Message: ")
-
-# Replace spaces with three periods
-user_input = user_input.replace(" ", "...")
-
-print(user_input)
